3afafe7eba31b5995e58b1fb19c9738a82794ea9
parkgeonwoong <hk77kh@gmail.com>
2023-11-12 14:20:56 +0900

    feat : [날짜객체] 크리스마스 디데이 할인 생성

--- src/constants/EventSetting.js
+++ src/constants/EventSetting.js
@@ -7,3 +7,9 @@ export const THIS_MONTH = Object.freeze({
   FIRST_DATE: 1,
   LAST_DATE: new Date(EVENT_DATE.YEAR, EVENT_DATE.MONTH + 1, 0).getDate(),
 });
+
+export const CALENDER = Object.freeze({
+  DATE: 'date',
+  DAY: 'day',
+  CHRISTMAS_DISCOUNT: 'christmasDiscount',
+});
--- src/model/Calender.js
+++ src/model/Calender.js
@@ -1,18 +1,25 @@
-import { EVENT_DATE, THIS_MONTH } from '../constants/EventSetting.js';
+import { CALENDER, EVENT_DATE, THIS_MONTH } from '../constants/EventSetting.js';
 
 export class Calender {
-  dateList = Array.from({ length: THIS_MONTH.LAST_DATE }, () => ({}));
+  calender = Array.from({ length: THIS_MONTH.LAST_DATE }, () => ({}));
 
   constructor() {
-    this.makeDateList();
+    this.makeCalender();
+    this.makeChristmasDiscount();
   }
 
-  makeDateList() {
-    this.dateList.forEach((_, i) => {
+  makeCalender() {
+    this.calender.forEach((_, i) => {
       const getDate = new Date(EVENT_DATE.YEAR, EVENT_DATE.MONTH, i + 1).getDate();
       const getDay = new Date(EVENT_DATE.YEAR, EVENT_DATE.MONTH, i + 1).getDay();
-      this.dateList[i]['date'] = getDate;
-      this.dateList[i]['day'] = getDay;
+      this.calender[i][CALENDER.DATE] = getDate;
+      this.calender[i][CALENDER.DAY] = getDay;
+    });
+  }
+
+  makeChristmasDiscount() {
+    this.calender.forEach((date, i) => {
+      this.calender[i][CALENDER.CHRISTMAS_DISCOUNT] = date.date < 26 ? 1000 + i * 100 : 0;
     });
   }
 }
