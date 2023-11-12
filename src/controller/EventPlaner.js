10d744d36d6da3271659b4873d72fa191a7bf82a
parkgeonwoong <hk77kh@gmail.com>
2023-11-12 16:10:11 +0900

    feat : [메뉴 상수] 메뉴 객체 생성

--- src/controller/EventPlaner.js
+++ src/controller/EventPlaner.js
@@ -2,11 +2,11 @@ import { Calender } from '../model/Calender.js';
 
 export class EventPlaner {
   constructor() {
+    // TODO: 모든캘린더가 아니라 입력받은 날짜만 메세지로 전달할 필요, 서비스 로직을 추가할 필요
     this.calender = new Calender();
   }
 
   async run() {
     console.log('getter: ', this.calender.getCalender());
-    console.log('privateTest: ', this.calender);
   }
 }
